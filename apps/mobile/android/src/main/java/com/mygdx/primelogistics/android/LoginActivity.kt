2910e3d03ee20d1a853a458c6b83413593af6bac
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-03-24 18:02:19 +0100

    feat: create activity login

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/LoginActivity.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/LoginActivity.kt
@@ -12,7 +12,7 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_login)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
