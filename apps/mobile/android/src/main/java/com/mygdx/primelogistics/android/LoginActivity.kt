2c66ee83465488c35a79cb47c8df80a6da558530
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-03-26 16:24:30 +0100

    feat: update login activity

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/LoginActivity.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.mygdx.primelogistics.android
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,14 +10,25 @@ import androidx.core.view.WindowInsetsCompat
 import com.mygdx.primelogistics.R
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var etUsername: EditText
+    lateinit var etPassword: EditText
+    lateinit var btnLogin: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_login)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        definirComponente()
+        btnLogin.setOnClickListener {
         }
+
     }
+
+    fun definirComponente(){
+        etUsername = findViewById<EditText>(R.id.etUsername)
+        etPassword = findViewById<EditText>(R.id.etPassword)
+        btnLogin = findViewById<Button>(R.id.btnLogin)
+    }
+
+
 }
