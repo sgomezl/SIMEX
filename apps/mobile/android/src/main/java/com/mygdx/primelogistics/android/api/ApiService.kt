cb3c32e2af66eb7c79d87bac5ccb3f647bff76ca
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-03-28 22:58:15 +0100

    feat: create api interface

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/api/ApiService.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/api/ApiService.kt
@@ -2,6 +2,7 @@ package com.mygdx.primelogistics.android.api
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 import com.mygdx.primelogistics.android.models.LoginRequest
 import com.mygdx.primelogistics.android.models.User
@@ -43,11 +44,27 @@ interface ApiService {
     suspend fun getUsers(@Header("Authorization") authorization: String): Response<List<User>>
 >>>>>>> 2f61192 (feat: create api interface)
 =======
+=======
+
+import com.mygdx.primelogistics.android.models.LoginRequest
+>>>>>>> 2f61192 (feat: create api interface)
 import com.mygdx.primelogistics.android.models.User
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface ApiService {
+    @POST("login")
+    suspend fun login(@Body request: LoginRequest): Response<ResponseBody>
+
     @GET("users")
+<<<<<<< HEAD
     suspend fun getUsers(): List<User>
 >>>>>>> f20535f (feat: Create ApiService.kt and RetrofitClient.kt)
+=======
+    suspend fun getUsers(@Header("Authorization") authorization: String): Response<List<User>>
+>>>>>>> 2f61192 (feat: create api interface)
 }
