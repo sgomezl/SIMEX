37ea9ec27a14efbc8507d25f94ce1383d2edbf97
Chenxiao Xu <cxu2223@politecnics.barcelona>
2026-04-24 15:57:22 +0200

    feat: ui activity_propuesta

--- apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/PropuestaActivity.kt
+++ apps/mobile/android/src/main/java/com/mygdx/primelogistics/android/PropuestaActivity.kt
@@ -7,12 +7,12 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.mygdx.primelogistics.R
 
-class DetalleOperacionOperatorActivity : AppCompatActivity() {
+class PropuestaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detalle_operacion_operator)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainActivityOperacionOperator)) { v, insets ->
+        setContentView(R.layout.activity_propuesta)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainPropuesta)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
